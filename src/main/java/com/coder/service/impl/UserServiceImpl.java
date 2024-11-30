05980dd4913f2a4a47369ba5c269f987983e5c8c
Shivam <shivam09084@gmail.com>
2024-11-30 14:42:04 +0530

    email send logic implement

--- src/main/java/com/coder/service/impl/UserServiceImpl.java
+++ src/main/java/com/coder/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import com.coder.dto.EmailRequest;
 import com.coder.dto.UserDto;
 import com.coder.entity.Role;
 import com.coder.entity.User;
@@ -30,8 +31,11 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private ModelMapper mapper;
 	
+	@Autowired
+	private EmailService emailService;
+	
 	@Override
-	public Boolean register(UserDto userDto) {
+	public Boolean register(UserDto userDto) throws Exception {
 		
 		validation.userValidation(userDto);
 		User user = mapper.map(userDto, User.class);
@@ -39,11 +43,30 @@ public class UserServiceImpl implements UserService {
 		setRole(userDto, user);
 		User  saveUser = userRepo.save(user);
 		if(!ObjectUtils.isEmpty(saveUser)) {
+			
+			emailSend(saveUser);
 			return true;
 		}
 		return false;
 	}
 
+	private void emailSend(User saveUser) throws Exception {
+		
+		String message = "Hi , <b>"+saveUser.getFirstName()+"</b>"
+									+"<br> Your Account Register Successfully.<br>"
+									+"<br> click the below link verify your Account <br>"
+									+"<a href='#'> click here </a>"
+									+"<br> Thanks & Regards<br>"
+									+"<br> Enotes<br>";
+		
+		EmailRequest emailRequest = new EmailRequest().builder()
+									.to(saveUser.getEmail())
+									.title("Account Creation ")
+									.subject("Registeration Successfully")
+									.message(message)
+									.build();
+		emailService.sendEmail(emailRequest);
+	}
 	private void setRole(UserDto userDto, User user) {
 		
 		List<Integer> reqRoleId = userDto.getRoles().stream().map(r->r.getId()).toList();
